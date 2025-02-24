d139feb7143db96700fe354db7403aafff397693
AntonSchellhorn <Jackpot.8.8.8.8.8A@gmail.com>
2025-02-24 18:09:01 +0100

    Update Lift.java

--- src/main/java/org/telran/lecture_11_greedy_practice/practice/hw/Lift.java
+++ src/main/java/org/telran/lecture_11_greedy_practice/practice/hw/Lift.java
@@ -17,4 +17,115 @@ package org.telran.lecture_11_greedy_practice.practice.hw;
 
 
 public class Lift {
+    private String name;
+
+    private int currentFloor;
+
+    private int speed;
+
+    public Lift(String name, int currentFloor, int speed) {
+        this.name = name;
+        this.currentFloor = currentFloor;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getArrivalTime(int targetFloor) {
+        return Math.abs(currentFloor - targetFloor) * speed;
+    }
 }
+package org.telran.lecture_11_greedy_practice.practice.hw;
+
+import java.util.List;
+
+public class LiftSystem {
+    private List<Lift> lifts;
+
+    public LiftSystem(List<Lift> lifts) {
+        this.lifts = lifts;
+    }
+
+    public Lift getFastestLift(int targetFloor) {
+        Lift fastestLift = lifts.get(0);
+        int minTime = fastestLift.getArrivalTime(targetFloor);
+
+        for (Lift lift : lifts) {
+            int time = lift.getArrivalTime(targetFloor);
+            if (time < minTime) {
+                fastestLift = lift;
+                minTime = time;
+            }
+        }
+        return fastestLift;
+    }
+}
+package org.telran.lecture_11_greedy_practice.practice.hw;
+
+import java.util.List;
+
+public class LiftSystem {
+    private List<Lift> lifts;
+
+    public LiftSystem(List<Lift> lifts) {
+        this.lifts = lifts;
+    }
+
+    public Lift getFastestLift(int targetFloor) {
+        Lift fastestLift = lifts.get(0);
+        int minTime = fastestLift.getArrivalTime(targetFloor);
+
+        for (Lift lift : lifts) {
+            int time = lift.getArrivalTime(targetFloor);
+            if (time < minTime) {
+                fastestLift = lift;
+                minTime = time;
+            }
+        }
+        return fastestLift;
+    }
+}
+package org.telran.lecture_11_greedy_practice.practice.hw;
+
+import java.util.Arrays;
+
+public class LiftApp {
+
+    public static void main(String[] args) {
+        Lift liftA = new Lift("A",4,3);
+        Lift liftB = new Lift("B",8,2);
+        Lift liftC = new Lift("C",5,1);
+        Lift liftD = new Lift("D",9,4);
+
+        LiftSystem liftSystem = new LiftSystem(Arrays.asList(liftA, liftB, liftC, liftD));
+
+        int targetFloor = 1;
+        Lift fastestLift = liftSystem.getFastestLift(targetFloor);
+
+        System.out.println("Отправляем лифт " + fastestLift.getName());
+    }
+}
+
+
